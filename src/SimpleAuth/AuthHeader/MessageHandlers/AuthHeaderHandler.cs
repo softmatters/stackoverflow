711db01d652ae25dba9b6070b9c6f32c83cb8dba
Shahzad Hassan <shahzad.hassan@softmatters.co.uk>
2019-04-25 09:55:26 +0100

    Added an ILogger to the AuthHeaderHandler

--- src/SimpleAuth/AuthHeader/MessageHandlers/AuthHeaderHandler.cs
+++ src/SimpleAuth/AuthHeader/MessageHandlers/AuthHeaderHandler.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace AuthHeader.MessageHandlers
 {
@@ -11,10 +12,12 @@ namespace AuthHeader.MessageHandlers
     /// </summary>
     public class AuthHeaderHandler : DelegatingHandler
     {
+        private readonly ILogger<AuthHeaderHandler> _logger;
         private readonly HttpContext _httpContext;
 
-        public AuthHeaderHandler(IHttpContextAccessor contextAccessor)
+        public AuthHeaderHandler(IHttpContextAccessor contextAccessor, ILogger<AuthHeaderHandler> logger)
         {
+            _logger = logger;
             // get hold of the HttpContext as we need to check if
             // the authorization header was passed in to the request
             _httpContext = contextAccessor.HttpContext;
@@ -22,13 +25,21 @@ namespace AuthHeader.MessageHandlers
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var headers = _httpContext?.Request.Headers["Authorization"];
+            if (_httpContext == null)
+            {
+                return await base.SendAsync(request, cancellationToken);
+            }
 
-            var authHeader = headers?.Any() == true ? headers.Value.First() : string.Empty;
+            var headers = _httpContext.Request.Headers["Authorization"];
+
+            var authHeader = headers.Any() ? headers.First() : string.Empty;
 
             // check if we have authorization in the incoming request header
             if (!string.IsNullOrEmpty(authHeader))
             {
+                // log the info if needed
+                _logger.LogInformation($"The request {_httpContext.Request.Path.ToUriComponent()} was called with header {authHeader}");
+
                 // add it to the outgoing API request header via HttpClient
                 request.Headers.Add("Authorization", $"Bearer {authHeader}");
             }
